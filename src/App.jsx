9708a1d292c34511b0fe4a0268ceaf9a0bed9fbd
Michael <segunmichael24@gmail.com>
2025-03-17 13:13:29 +0100

    feat: add forgot password page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Button } from "@/components/ui/button"
 import { Container } from "@/components/ui/container"
 import { Home } from "lucide-react"
+import { Input } from "./components/ui/input";
 
 function App() {
   return (
     <Container>
-      <div className="flex flex-col items-center justify-center min-h-svh">
-        <Home size={64} />
-        <Button>Click me</Button>
+      <div className="">
+        <p>Forgot Password?</p>
+        <p>Enter your email address, and we'll send you a link to reset your password.</p>
+      </div>
+
+      <div>
+        <Input type="email" placeholder="Email" />
+        <Button className="w-full">Button</Button>
       </div>
     </Container>
   )
