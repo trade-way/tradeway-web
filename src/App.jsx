9255b3b95432b021199daf19f0cd55a2b4574e86
Ooreoluwa <goodnessoluseyi@gmail.com>
2025-03-19 16:25:36 +0100

    Updated Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import PrivateRoute from './lib/privateRoute.jsx'
+import Login from './pages/Login.jsx'
 import { Container } from './components/ui/container.jsx'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <BrowserRouter>
             <Routes>
               {/* SignIn, SignUp and landing page routes will be here */}
+              <Route path='/login' element={Login}/>
               <Route element={<PrivateRoute/>}>
               {/* The layout route */}
               {/* The routing that will be protected will be here */}
