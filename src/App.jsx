ce466aa3b6ca074f7d51b94069a6023433373979
Abudu Rodiyyah <rodiyahabudu@gmail.com>
2025-03-25 08:50:00 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,24 @@ import { Container } from "@/components/ui/container"
 function App() {
   return (
     <Container>
-     home
+      <BrowserRouter>
+        <Routes>
+          {/* SignIn, SignUp and landing page routes will be here */}
+          <Route path="/login" element={<Login/>} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/Pass" element={<Pass />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route
+            path="*"
+            element={<div>Page not found: {window.location.pathname}</div>}
+          />
+          <Route element={<PrivateRoute />}>
+            {/* The layout route */}
+            {/* The routing that will be protected will be here */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </Container>
 
 
